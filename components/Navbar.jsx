6bb234bc581c043753b4933144eadfde4a26d5f0
GigaKF <gigakurnia81@gmail.com>
2025-04-30 21:43:21 +0700

    Initial commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react";
-import { assets } from "@/assets/assets";
+import { assets, BagIcon, BoxIcon, CartIcon, HomeIcon } from "@/assets/assets";
 import Link from "next/link"
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
@@ -47,7 +47,14 @@ const Navbar = () => {
           height={16}
         />
         {user ? (
-          <UserButton afterSignOutUrl="/" />
+          <UserButton> 
+            <UserButton.MenuItems>
+              <UserButton.Action label="Cart" labelIcon={<CartIcon />} onClick={()=> router.push('/cart')} />
+            </UserButton.MenuItems>
+            <UserButton.MenuItems>
+              <UserButton.Action label="My Orders" labelIcon={<BagIcon />} onClick={()=> router.push('/my-orders')} />
+            </UserButton.MenuItems>
+          </UserButton>
         ) : (
           <button 
             onClick={openSignIn} 
@@ -67,7 +74,20 @@ const Navbar = () => {
       <div className="flex items-center md:hidden gap-3">
         {isSeller && <button onClick={() => router.push('/seller')} className="text-xs border px-4 py-1.5 rounded-full">Seller Dashboard</button>}
         {user ? (
-          <UserButton afterSignOutUrl="/" />
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action label="Home" labelIcon={<HomeIcon />} onClick={()=> router.push('/')} />
+            </UserButton.MenuItems>
+            <UserButton.MenuItems>
+              <UserButton.Action label="Products" labelIcon={<BoxIcon />} onClick={()=> router.push('/all-products')} />
+            </UserButton.MenuItems>
+            <UserButton.MenuItems>
+              <UserButton.Action label="Cart" labelIcon={<CartIcon />} onClick={()=> router.push('/cart')} />
+            </UserButton.MenuItems>
+            <UserButton.MenuItems>
+              <UserButton.Action label="My Orders" labelIcon={<BagIcon />} onClick={()=> router.push('/my-orders')} />
+            </UserButton.MenuItems>
+          </UserButton>
         ) : (
           <button 
             onClick={openSignIn} 
